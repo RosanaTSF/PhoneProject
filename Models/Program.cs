a8ec0dc83b837285357853b0a6bd2e98b92f90e1
Rosana <rosanatsf@gmail.com>
2024-01-05 13:39:21 -0300

    Comentarios provisórios para estudo.

--- Models/Program.cs
+++ Models/Program.cs
@@ -2,7 +2,6 @@
 using DesafioPOO.Models; // Lógica: Importa o namespace DesafioPOO.Models para utilizar a classe base Smartphone.
 using PhonePooProject.Models; // Lógica: Importa o namespace PhonePooProject.Models para utilizar as classes Flip e Iphone.
 
-
 namespace PhonePooProject // Lógica: Define o namespace para a classe Program no contexto do projeto PhonePooProject.
 {
     class Program 
@@ -14,8 +13,7 @@ namespace PhonePooProject // Lógica: Define o namespace para a classe Program n
             Iphone i = new Iphone("111222333", "ModelIphone", "IMEIIphone", 1024); // Lógica: Cria uma instância da classe Iphone chamada i com valores específicos passados como parâmetros para o construtor.
             i.InstalarAplicativo("AppIphone"); // Saída: Mensagem formatada na console. Lógica: Chama o método InstalarAplicativo da instância i (objeto da classe Iphone), indicando que um aplicativo está sendo instalado no iPhone.
 
-            Console.ReadLine(); // Lógica: Aguarda a entrada do usuário antes de encerrar a execução do programa.
-            
+            Console.ReadLine(); // Lógica: Aguarda a entrada do usuário antes de encerrar a execução do programa.    
         }
     }
 }
